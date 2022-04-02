a39f498168085614b9c49369b6dd0bc920b9a73e
eitan baron <eitanbaron@outlook.com>
2022-04-02 19:17:47 +0300

    1

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 /* eslint no-console: 0 */
 
-'use strict';
-const nodemailer = require('nodemailer');
-const fs = require('fs')
-const cors = require('cors');
-const express = require('express');
+"use strict";
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+const cors = require("cors");
+const express = require("express");
 const app = express(),
-      bodyParser = require("body-parser"),
-      port = 3080;
+  bodyParser = require("body-parser"),
+  port = 3080;
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 // place holder for the data
 // const users = ['u1','u2','u3'];
 
@@ -22,18 +24,18 @@ app.use(bodyParser.json());
 //   res.json(users);
 // });
 
-app.post('/api/sendemail', (req, res) => {
+app.post("/api/sendemail", (req, res) => {
   const email = req.body.email;
-  console.log('sending email');  
-  buildAndSendEmail(email,res); 
+  console.log("sending email");
+  buildAndSendEmail(email, res);
 });
 
-app.get('/', (req,res) => {
-    res.send('App Works !!!!');
+app.get("/", (req, res) => {
+  res.send("App Works !!!!");
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+  console.log(`Server listening on the port::${port}`);
 });
 
 // const chokidar = require('chokidar');
@@ -42,30 +44,30 @@ app.listen(port, () => {
 //   persistent: true
 // });
 
-// watcher  
+// watcher
 //   .on('change', path => {
 //       console.log(`File ${path} has been changed`)
 //       sendEmail()
-//     })  
+//     })
 
-function buildAndSendEmail(html,res){
-    let emailTemplatePath = "c:/Users/eitan/Desktop/email-test-template.html";
-    // let sendFilePath = "c:/Users/eitan/Desktop/email-test.html";
-    fs.readFile(emailTemplatePath, "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      
-      let fullEmail = data.replace("<--content-->", html);
-      sendEmail(fullEmail,res);
+function buildAndSendEmail(html, res) {
+  let emailTemplatePath = "./template/email-test-template.html";
+  // let sendFilePath = "c:/Users/eitan/Desktop/email-test.html";
+  fs.readFile(emailTemplatePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    let fullEmail = data.replace("<--content-->", html);
+    sendEmail(fullEmail, res);
     //   fs.writeFile(sendFilePath, this.fileContent, (err) => {
     //     if (err) {
     //       console.error(err);
     //       return;
     //     }
     //   });
-    });
+  });
 }
 
 // fs.watch('c:/Users/eitan/Desktop/email-test.html', function() {
@@ -73,62 +75,62 @@ function buildAndSendEmail(html,res){
 //     sendEmail()
 // });
 
-function sendEmail(data,res){
-    // fs.readFile('c:/Users/eitan/Desktop/email-test.html', 'utf8' , (err, data) => {
-    //     if (err) {
-    //         console.error(err)
-    //         return
-    //     }        
-        
-        // Generate SMTP service account from ethereal.email
-    nodemailer.createTestAccount((err) => {
-        if (err) {
-            console.error('Failed to create a testing account');
-            console.error(err);
-            return process.exit(1);
-        }
+function sendEmail(data, res) {
+  // fs.readFile('c:/Users/eitan/Desktop/email-test.html', 'utf8' , (err, data) => {
+  //     if (err) {
+  //         console.error(err)
+  //         return
+  //     }
 
-        console.log('Credentials obtained, sending message...');
+  // Generate SMTP service account from ethereal.email
+  nodemailer.createTestAccount((err) => {
+    if (err) {
+      console.error("Failed to create a testing account");
+      console.error(err);
+      return process.exit(1);
+    }
 
-        // Create a SMTP transporter object
-        let transporter = nodemailer.createTransport(
-            {
-                service: 'gmail',
-                host: 'smtp.gmail.com',
-                auth: {
-                    user: 'eitan2007@gmail.com',
-                    pass: 'gnnjozexfwnmwewi'
-                },
-                logger: true,
-                transactionLog: true // include SMTP traffic in the logs
-            },
-            {
-                // default message fields
+    console.log("Credentials obtained, sending message...");
 
-                // sender info
-                from: 'Nodemailer <example@nodemailer.com>',
-                headers: {
-                    'X-Laziness-level': 1000 // just an example header, no need to use this
-                }
-            }
-        );
+    // Create a SMTP transporter object
+    let transporter = nodemailer.createTransport(
+      {
+        service: "gmail",
+        host: "smtp.gmail.com",
+        auth: {
+          user: "eitan2007@gmail.com",
+          pass: "gnnjozexfwnmwewi",
+        },
+        logger: true,
+        transactionLog: true, // include SMTP traffic in the logs
+      },
+      {
+        // default message fields
 
-        // Message object
-        let message = {
-            // Comma separated list of recipients
-            to: 'eitan2007@gmail.com',
+        // sender info
+        from: "Nodemailer <example@nodemailer.com>",
+        headers: {
+          "X-Laziness-level": 1000, // just an example header, no need to use this
+        },
+      }
+    );
 
-            // Subject of the message
-            subject: 'Nodemailer is unicode friendly ✔' + Date.now(),
+    // Message object
+    let message = {
+      // Comma separated list of recipients
+      to: "eitan2007@gmail.com",
 
-            // // plaintext body
-            // text:'fghfghfg',
+      // Subject of the message
+      subject: "Nodemailer is unicode friendly ✔" + Date.now(),
 
-            // HTML body
-            html: data,
+      // // plaintext body
+      // text:'fghfghfg',
 
-            // AMP4EMAIL
-            amp: `<!doctype html>
+      // HTML body
+      html: data,
+
+      // AMP4EMAIL
+      amp: `<!doctype html>
             <html ⚡4email>
             <head>
                 <meta charset="utf-8">
@@ -143,69 +145,65 @@ function sendEmail(data,res){
             </body>
             </html>`,
 
-            // An array of attachments
-            attachments: [
-                // String attachment
-                // {
-                //     filename: 'notes.txt',
-                //     content: 'Some notes about this e-mail',
-                //     contentType: 'text/plain' // optional, would be detected from the filename
-                // },
+      // An array of attachments
+      attachments: [
+        // String attachment
+        // {
+        //     filename: 'notes.txt',
+        //     content: 'Some notes about this e-mail',
+        //     contentType: 'text/plain' // optional, would be detected from the filename
+        // },
 
-                // Binary Buffer attachment
-                {
-                    filename: 'image1.png',
-                    content: Buffer.from(
-                        '',
-                        'base64'
-                    ),
+        // Binary Buffer attachment
+        {
+          filename: "image1.png",
+          content: Buffer.from("", "base64"),
 
-                    cid: 'image1' // should be as unique as possible
-                },
+          cid: "image1", // should be as unique as possible
+        },
 
-                // File Stream attachment
-                // {
-                //     filename: 'nyan cat ✔.gif',
-                //     path: __dirname + '/assets/nyan.gif',
-                //     cid: 'nyan@example.com' // should be as unique as possible
-                // }
-            ],
+        // File Stream attachment
+        // {
+        //     filename: 'nyan cat ✔.gif',
+        //     path: __dirname + '/assets/nyan.gif',
+        //     cid: 'nyan@example.com' // should be as unique as possible
+        // }
+      ],
 
-            // list: {
-            //     // List-Help: <mailto:admin@example.com?subject=help>
-            //     help: 'admin@example.com?subject=help',
+      // list: {
+      //     // List-Help: <mailto:admin@example.com?subject=help>
+      //     help: 'admin@example.com?subject=help',
 
-            //     // List-Unsubscribe: <http://example.com> (Comment)
-            //     unsubscribe: [
-            //         {
-            //             url: 'http://example.com/unsubscribe',
-            //             comment: 'A short note about this url'
-            //         },
-            //         'unsubscribe@example.com'
-            //     ],
+      //     // List-Unsubscribe: <http://example.com> (Comment)
+      //     unsubscribe: [
+      //         {
+      //             url: 'http://example.com/unsubscribe',
+      //             comment: 'A short note about this url'
+      //         },
+      //         'unsubscribe@example.com'
+      //     ],
 
-            //     // List-ID: "comment" <example.com>
-            //     id: {
-            //         url: 'mylist.example.com',
-            //         comment: 'This is my awesome list'
-            //     }
-            // }
-        };
+      //     // List-ID: "comment" <example.com>
+      //     id: {
+      //         url: 'mylist.example.com',
+      //         comment: 'This is my awesome list'
+      //     }
+      // }
+    };
 
-        transporter.sendMail(message, (error, info) => {
-            if (error) {
-                console.log('Error occurred');
-                console.log(error.message);
-                return process.exit(1);
-            }
-            
-            res.json("success");
-            console.log('Message sent successfully!');
-            console.log(nodemailer.getTestMessageUrl(info));
+    transporter.sendMail(message, (error, info) => {
+      if (error) {
+        console.log("Error occurred");
+        console.log(error.message);
+        return process.exit(1);
+      }
 
-            // only needed when using pooled connections
-            transporter.close();    
-        });
+      res.json("success");
+      console.log("Message sent successfully!");
+      console.log(nodemailer.getTestMessageUrl(info));
 
-    });    
+      // only needed when using pooled connections
+      transporter.close();
+    });
+  });
 }
